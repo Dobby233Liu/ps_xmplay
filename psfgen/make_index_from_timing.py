126d9b35e2745511df7646ec7070dbba90b76876
Liu Wenyuan <15816141883@163.com>
2024-08-27 17:11:04 +0800

    Rename like in the set

--- psfgen/make_index_from_timing.py
+++ psfgen/make_index_from_timing.py
@@ -13,9 +13,13 @@ def main():
             continue
         with open(timing_fn, "rb") as timing_f:
             timing_fnn = path.splitext(path.basename(timing_fn))[0]
+            timing_fnn2 = timing_fnn
+            #torment = glob.glob(f"out/{SONGDATA_DIR}/*{timing_fnn}.psf")
+            #if len(torment) > 0:
+            #    timing_fnn2 = path.splitext(path.basename(torment[0]))[0]
             timing = libopenmpt.Module(timing_f)
             for i in range(timing.num_subsongs):
-                minipsf_fn = timing_fnn + ("_" + str(i) if timing.num_subsongs != 1 else "")
+                minipsf_fn = timing_fnn2 + ("_" + str(i) if timing.num_subsongs != 1 else "")
                 index[minipsf_fn] = {
                     "xm": timing_fnn,
                     "module_ext": path.splitext(timing_fn)[1][1:],
