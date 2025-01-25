47105f364bf7fcba0a5b89a0997e6d4e2de5e539
Liu Wenyuan <15816141883@163.com>
2025-01-25 23:58:33 +0800

    fixup! ^^

--- psfgen/gen.py
+++ psfgen/gen.py
@@ -56,7 +56,7 @@ def main():
                 print(song_name)
 
             # TODO: variant setting should not be here
-            lib = modify_driver._make_psflib_elf(info["xm"], SONGDATA_DIR, info.get("xmplay_variant", XMPLAY_VARIANT), info.get("worse_timing", False))
+            lib = modify_driver._make_psflib_elf(info["xm"], SONGDATA_DIR, info.get("xmplay_variant", XMPLAY_VARIANT))
             if not making_psf:
                 lib_psf = modify_driver.make_psflib_psf(lib)
                 with open(f"{outdir}/{lib_fn}", "wb") as libf:
