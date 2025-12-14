e23ec27b6f4309ae02882782d311b8e7283815c8
Liu Wenyuan <15816141883@163.com>
2025-12-14 18:26:38 +0800

    Increase code clarity

--- xmdecompressor/main.py
+++ xmdecompressor/main.py
@@ -55,93 +55,102 @@ class XM_RowPackedFlag(IntFlag):
 
 
 # ported from: https://github.com/OpenDriver2/OpenDriver2Tools/blob/89009f7f4f48f37a886b376292432cdffab4a136/DriverSoundTool/driver_sound.cpp#L181
-# unreadability is preserved because idk what it's doing
+# unreadability is preserved because I don't quite understand what it's doing fully
 # samples are not restored because it doesn't matter here
 def decompress_xm(inf: BufferedReader, outf: BufferedWriter):
-    inf.seek(58)
+    ver_start = 58
+    inf.seek(ver_start)
     in_ver = unpack_io1(inf, "<H")
     inf.seek(0)
-
     if in_ver != 0xDDBA:
         print("This is a non-superpacked XM")
         outf.write(inf.read())
         return
 
-    outf.write(inf.read(336))  # go to patterns
-    poke(outf, 58, pack("<H", 0x104))  # convert XM to standard version
+    outf.write(inf.read(336))  # until pattern data
+    poke(outf, ver_start, pack("<H", 0x104))  # convert XM to standard version
 
     inf.seek(68)
     num_chnl, num_pat = unpack_io(inf, "<HH")
-    inf.seek(264, os.SEEK_CUR)  # skip other header data since it's not changed
+    inf.seek(264, os.SEEK_CUR)  # skip other header data since they don't change
     print("Unpacking", num_pat, "patterns")
 
-    for i in range(num_pat):
+    for pat in range(num_pat):
         # copy pattern header properties
         outf.write(inf.read(5))
-        pat_len = unpack_io1(inf, "<h")
-        outf.write(pack("<h", pat_len))
+        num_rows = unpack_io1(inf, "<h")
+        outf.write(pack("<h", num_rows))
 
         pat_size = unpack_io1(inf, "<h")  # onto pattern data itself
-        out_pat_size_start = outf.tell()
+        final_pat_size_start = outf.tell()
         outf.write(pack("<h", pat_size))
 
         if pat_size == 0:
             continue  # no empty reads
 
         pat_start_in, pat_start_out = inf.tell(), outf.tell()
-        for r in range(pat_len):
-            row_notes = (XM_Note * max(32, num_chnl))()  # more for safety
 
+        for row in range(num_rows):
+            # all inited to 0
+            notes_per_chnl = (XM_Note * max(32, num_chnl))()  # more for safety
+
+            # apparently instead of simply laying out all channels per row, "super-packed" XM
+            # only lays out channels that have note data?
+            # read notes of layed out channels
             while (inf.tell() - pat_start_in) < pat_size:  # don't overflow reading
-                patdat = unpack_io1(inf, "<B")
-                if patdat == 0xFF:  # channel end
+                chnl = unpack_io1(inf, "<B")
+                if chnl == 0xFF:  # end of data
                     break
 
-                xmnote = row_notes[patdat]
-
-                # unpack it fully
+                # unpack note
+                note_struct = notes_per_chnl[chnl]
                 note = unpack_io1(inf, "<B")
                 if note & XM_RowPackedFlag.packed:
-                    xmnote.note = unpack_io1(inf, "<B") if note & XM_RowPackedFlag.note else 0
-                    xmnote.inst = unpack_io1(inf, "<B") if note & XM_RowPackedFlag.inst else 0
-                    xmnote.volc = unpack_io1(inf, "<B") if note & XM_RowPackedFlag.volc else 0
-                    xmnote.efft = unpack_io1(inf, "<B") if note & XM_RowPackedFlag.efft else 0
-                    xmnote.effp = unpack_io1(inf, "<B") if note & XM_RowPackedFlag.effp else 0
+                    note_struct.note = unpack_io1(inf, "<B") if note & XM_RowPackedFlag.note else 0
+                    note_struct.inst = unpack_io1(inf, "<B") if note & XM_RowPackedFlag.inst else 0
+                    note_struct.volc = unpack_io1(inf, "<B") if note & XM_RowPackedFlag.volc else 0
+                    note_struct.efft = unpack_io1(inf, "<B") if note & XM_RowPackedFlag.efft else 0
+                    note_struct.effp = unpack_io1(inf, "<B") if note & XM_RowPackedFlag.effp else 0
                 else:
-                    xmnote.note = note
-                    xmnote.inst, xmnote.volc, xmnote.efft, xmnote.effp = unpack_io(inf, "<BBBB")
+                    note_struct.note = note
+                    note_struct.inst, note_struct.volc, note_struct.efft, note_struct.effp = unpack_io(inf, "<BBBB")
 
-            # pack notes again
-            for c in range(num_chnl):
-                xmnote: XM_Note = row_notes[c]
-
-                if xmnote.note > 0 and xmnote.inst > 0 and xmnote.volc > 0 and xmnote.efft > 0 and xmnote.effp > 0:
+            # then pack notes, this time for all channels
+            for chnl in range(num_chnl):
+                note_struct: XM_Note = notes_per_chnl[chnl]
+                if (
+                    note_struct.note > 0
+                    and note_struct.inst > 0
+                    and note_struct.volc > 0
+                    and note_struct.efft > 0
+                    and note_struct.effp > 0
+                ):
                     # write full note
-                    outf.write(bytes(xmnote))
+                    outf.write(bytes(note_struct))
                 else:
                     packed_flag = XM_RowPackedFlag.packed
                     packed_flag_start = outf.tell()
                     outf.write(pack("<B", packed_flag))
 
-                    if xmnote.note > 0:
+                    if note_struct.note > 0:
                         packed_flag |= XM_RowPackedFlag.note
-                        outf.write(pack("<B", xmnote.note))
-                    if xmnote.inst > 0:
+                        outf.write(pack("<B", note_struct.note))
+                    if note_struct.inst > 0:
                         packed_flag |= XM_RowPackedFlag.inst
-                        outf.write(pack("<B", xmnote.inst))
-                    if xmnote.volc > 0:
+                        outf.write(pack("<B", note_struct.inst))
+                    if note_struct.volc > 0:
                         packed_flag |= XM_RowPackedFlag.volc
-                        outf.write(pack("<B", xmnote.volc))
-                    if xmnote.efft > 0:
+                        outf.write(pack("<B", note_struct.volc))
+                    if note_struct.efft > 0:
                         packed_flag |= XM_RowPackedFlag.efft
-                        outf.write(pack("<B", xmnote.efft))
-                    if xmnote.effp > 0:
+                        outf.write(pack("<B", note_struct.efft))
+                    if note_struct.effp > 0:
                         packed_flag |= XM_RowPackedFlag.effp
-                        outf.write(pack("<B", xmnote.effp))
+                        outf.write(pack("<B", note_struct.effp))
 
                     poke(outf, packed_flag_start, pack("<B", packed_flag))
 
-        poke(outf, out_pat_size_start, pack("<h", outf.tell() - pat_start_out))
+        poke(outf, final_pat_size_start, pack("<h", outf.tell() - pat_start_out))
 
     # copy remaining data
     outf.write(inf.read())
